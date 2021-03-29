294c9a317a4544e9443b290f8897c6850a1d5cf9
tangmack <macktang@gmail.com>
2021-03-29 02:32:29 -0600

    initial commit, night image corrected but noisy

---
+++ histogram_eq_script.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+import numpy as np
+from scipy import ndimage
+
+cap = cv2.VideoCapture('night/Night Drive - 2689.mp4')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # blur = cv2.GaussianBlur(gray, (51, 51), 0)
+    # blur = cv2.medianBlur(gray,15)
+    equalized = cv2.equalizeHist(gray)
+    # blur = cv2.GaussianBlur(equalized, (9, 9), 0)
+    # blur = cv2.medianBlur(equalized,15)
+
+    cv2.imshow('frame',equalized)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
