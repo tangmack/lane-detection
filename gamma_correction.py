b27d2d85afda18c6bcda3905457cd239ffab9d43
tangmack <macktang@gmail.com>
2021-04-04 08:29:11 -0600

    gamma correction

---
+++ gamma_correction.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+gamma = 4.5
+
+cap = cv2.VideoCapture('night/Night Drive - 2689.mp4')
+# cap = cv2.VideoCapture('data_2/challenge_video.mp4')
+# cap = cv2.VideoCapture('data1.mp4')
+
+while (cap.isOpened()):
+    ret, frame_original = cap.read()
+    frame = cv2.cvtColor(frame_original, cv2.COLOR_BGR2GRAY)
+
+    # frame =  cv2.GaussianBlur(gray, (21, 21), 0)
+    # th3 = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+    #                            cv2.THRESH_BINARY, 11, -1)
+    # ret2, th3 = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # frame = cv2.GaussianBlur(frame_original, (3, 3), 0)
+    # adjusted = gamma_adjust(frame, gamma)
+
+    img_normalized = frame/255
+    gamma_inverse = 1/gamma
+    gamma_image = img_normalized**(gamma_inverse)
+
+
+    cv2.imshow("gamma_image", gamma_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
+cap.release()
